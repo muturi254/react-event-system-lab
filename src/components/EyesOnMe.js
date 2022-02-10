879badb1cc818590eea01c0d4e75bdf9376faa44
muturi25 <petermuturi74@gmail.com>
2022-02-11 00:48:13 +0300

    solution

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOmMe extends Component {
+
+    handleBlur = () => {
+        console.log("Hey! Eyes on me!");
+    }
+
+    handleFocus = () => {
+        console.log("Good!");
+    }
+
+
+    render() {
+        return (
+            <button onBlur={this.handleBlur} onFocus={this.handleFocus}>Try</button>
+        )
+    }
+}
+
+export default EyesOmMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+    handleKeyUp = (event) => {
+        console.log('Entering password...');
+    }
+
+    render() {
+        return (
+            <input type="password" onKeyUp={this.handleKeyUp} />
+        )
+    }
+}
+
+export default Keypad;
